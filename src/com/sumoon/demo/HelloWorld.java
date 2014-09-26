ce1b12c20f69ba136600297fd5094dd4bd5cd193
hongtongyue <814218621@qq.com>
2014-09-26 22:05:48 +0800

    为HelloWorld类添加sayHello方法

--- src/com/sumoon/demo/HelloWorld.java
+++ src/com/sumoon/demo/HelloWorld.java
@@ -3,6 +3,10 @@ package com.sumoon.demo;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("这是我的第一个HelloWorld小程序");
+	}
+	
+	public String sayHello() {
+		return "Hello Everyone!";
 	}
 }
